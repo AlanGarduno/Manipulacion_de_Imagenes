7f7c514cbf0155b608e6adad7e570c5940284797
Alan Garduño Velazquez <agv_lrd@hotmail.com>
2017-07-13 01:23:46 -0500

    Recortar imagen

--- image.py
+++ image.py
@@ -2,15 +2,26 @@ from PIL  import  Image
 
 try:
 	img = Image.open("image/im2.jpg") #abre la imagen, recibe la ruta 
+
 	#print(img.mode) #devuelve en que modo vectorial esta la imagen
-	#print(img.getpixel( (100,200) )) #Nuestra los colores de los pixeles
+
+	#print(img.getpixel( (100,200) )) #Muestra los colores de los pixeles
+
 	#img.convert("L") #Cambia la imagen a vlanco y negro
-	#img = img.rotate(40, expand = True) #rota la imagen, recibe cuantos grados se va a rotar, expand, modifica el tamano de la imagen 
+
+	#img = img.rotate(40, expand = True) #rota la imagen, 
+	#recibe cuantos grados se va a rotar, expand, modifica el tamano de la imagen
+
 	#img.save("image/news/im2.jpg") #Guarda la imagen en el disco, recibe la ruta
-	print(img.size) #obtener el tamano de la imagen
+
+	#print(img.size) #obtener el tamano de la imagen
 
 
-	img = img.resize( (100,200) ) #Redimenciona la imagen, recibe una tupla con ancho, alto
+	#img = img.resize( (100,200) ) #Redimenciona la imagen, recibe una tupla con ancho, alto
+
+	box = (300,100,400,900) 
+
+	img = img.crop(box) #Extrae la porcion de una imagen 
 
 	img.show() #abre la imagen 
 except IOError:
