b811655d0176d1fb8942b030dfab3154114e7bba
Alan Garduño Velazquez <agv_lrd@hotmail.com>
2017-07-13 01:09:56 -0500

    Rotacion

--- image.py
+++ image.py
@@ -5,8 +5,9 @@ try:
 	#print(img.mode) #devuelve en que modo vectorial esta la imagen
 	#print(img.getpixel( (100,200) )) #Nuestra los colores de los pixeles
 	img.convert("L") #Cambia la imagen a vlanco y negro
+	img = img.rotate(40, expand = True) #rota la imagen, recibe cuantos grados se va a rotar, expand, modifica el tamano de la imagen 
 	img.save("image/news/im2.jpg") #Guarda la imagen en el disco, recibe la ruta
 	
-	#img.show() #abre la imagen 
+	img.show() #abre la imagen 
 except IOError:
 	print("No es posible abrir la imagen")
