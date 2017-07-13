42d3257f493e3c868483dff75b7ff021e7285f2f
Alan Garduño Velazquez <agv_lrd@hotmail.com>
2017-07-13 00:53:02 -0500

    Agregando modos de imagen

--- image.py
+++ image.py
@@ -2,6 +2,10 @@ from PIL  import  Image
 
 try:
 	img = Image.open("image/im1.jpg")
-	img.show()
+	print(img.mode)
+	print(img.getpixel( (100,200) ))
+
+	img = img.convert("L")
+	#img.show()
 except IOError:
 	print("No es posible abrir la imagen")
