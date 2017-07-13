0d7fdec6bce1fcaa8dc1e3348faf52d3f19eb9c1
Alan Garduño Velazquez <agv_lrd@hotmail.com>
2017-07-13 01:03:00 -0500

    Guardar los cambios en disco

--- image.py
+++ image.py
@@ -1,11 +1,12 @@
 from PIL  import  Image
 
 try:
-	img = Image.open("image/im1.jpg")
-	print(img.mode)
-	print(img.getpixel( (100,200) ))
-
-	img = img.convert("L")
-	#img.show()
+	img = Image.open("image/im2.jpg") #abre la imagen, recibe la ruta 
+	#print(img.mode) #devuelve en que modo vectorial esta la imagen
+	#print(img.getpixel( (100,200) )) #Nuestra los colores de los pixeles
+	img.convert("L") #Cambia la imagen a vlanco y negro
+	img.save("image/news/im2.jpg") #Guarda la imagen en el disco, recibe la ruta
+	
+	#img.show() #abre la imagen 
 except IOError:
 	print("No es posible abrir la imagen")
